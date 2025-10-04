621b39f58d134bd35b574325907941c7c51c8688
Sheklein Aleksey <sheklein.al@yandex.ru>
2025-10-04 13:10:12 +0300

    1

---
+++ cmd/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	key := "7576443951:AAGEbz7-S8AmrF1-ZS-lIEaSFPXpVCjXlqc"
+	bot, err := tgbotapi.NewBotAPI(key)
+	if err != nil {
+		fmt.Println("Cannot init tgbot")
+	}
+	log.Printf("Auth on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+	for update := range updates {
+		if update.Message != nil {
+			text := update.Message.Text
+			chatID := update.Message.Chat.ID
+			userID := update.Message.From.ID
+			log.Printf("[%s](%d) %s", update.Message.From.UserName, userID, text)
+			// msg := tgbotapi.NewMessage(chatID, text)
+
+			switch text {
+			case "/start":
+				msg := tgbotapi.NewMessage(chatID, "Привет! Я бот. Используй /help для списка команд")
+				bot.Send(msg)
+
+			case "/help":
+				helpText := `Доступные команды:
+/start - начать работу
+/help - помощь
+/info - информация о боте
+/about - о разработчике`
+				msg := tgbotapi.NewMessage(chatID, helpText)
+				bot.Send(msg)
+
+			case "/info":
+				msg := tgbotapi.NewMessage(chatID, "Я простой Telegram бот на Go")
+				bot.Send(msg)
+
+			case "/calc":
+				msg := tgbotapi.NewMessage(chatID, "Разработано с использованием tgbotapi")
+				bot.Send(msg)
+
+			default:
+				// Если это не команда, делаем эхо
+				msg := tgbotapi.NewMessage(chatID, text)
+				bot.Send(msg)
+			}
+
+			// bot.Send(msg)
+		}
+	}
+
+}
